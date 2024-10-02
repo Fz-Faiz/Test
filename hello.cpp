04902d15bd98a7c830d52988ea94d6adc5ecfc43
Faiz Ahmad <faizahmad43430@gmail.com>
2024-10-02 23:02:27 +0530

    g

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    vector<int> arr;
+    for(int i = 0;i<N;i++){
+        cin>>arr[i];
+        cout<<"\n";
+
+    }
+    for(int i =0;i<N;i++){
+        cout<<arr[i];
+    }
+}
